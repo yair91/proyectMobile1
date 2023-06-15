091e1d35bab7f9638a3c89c2e630888ba6e553d8
Jonathan Perez <jona88738@gmail.com>
2023-06-15 00:01:22 -0600

    Arreglado el bug del login y corregido el arreglo de usuarios

--- Proyecto/app/src/main/java/com/example/proyecto/Register_activity.java
+++ Proyecto/app/src/main/java/com/example/proyecto/Register_activity.java
@@ -10,7 +10,7 @@ import android.widget.Toast;
 
 public class Register_activity extends AppCompatActivity {
     private EditText nombre, apellido, correo, contrasena, confirmarContra;
-    Usuario user[];
+    Usuario user;
 
 
     @Override
@@ -23,7 +23,7 @@ public class Register_activity extends AppCompatActivity {
         correo = findViewById(R.id.editxtEmail);
         contrasena = findViewById(R.id.editxtPassword);
         confirmarContra = findViewById(R.id.etConfirmPassword);
-        user = (Usuario[]) getIntent().getSerializableExtra("usuarios");
+        user = (Usuario) getIntent().getSerializableExtra("usuarios");
     }
 
     public void registrarse(View view) {
@@ -33,25 +33,32 @@ public class Register_activity extends AppCompatActivity {
         String password = contrasena.getText().toString();
         String confirm = confirmarContra.getText().toString();
 
-        if (password.equals(confirm)) {
-            for (int i = 0; i < 15; i++) {
-                if (user[i].getNombre().equals("")) {
-                    user[i].setNombre(name);
-                    user[i].setApellido(lastName);
-                    user[i].setEmail(email);
-                    user[i].setContrasena(password);
+        if(name.equals("")  || lastName.equals("") || email.equals("") || password.equals("") || confirm.equals("")){
+            Toast mensaje = Toast.makeText(this, "Rellene todos los campos ", Toast.LENGTH_LONG);
+            mensaje.show();
+        }else{
+            if (password.equals(confirm)) {
+
+                if (user.getNombre().equals("")) {
+                    user.setNombre(name);
+                    user.setApellido(lastName);
+                    user.setEmail(email);
+                    user.setContrasena(password);
 
                     Intent intent = new Intent(Register_activity.this, MainActivity.class);
                     intent.putExtra("usuarios", user);
                     startActivity(intent);
-                    break;
+
                 }
+
+            } else {
+                Toast mensaje = Toast.makeText(this, "Las contraseña no coinciden", Toast.LENGTH_LONG);
+                mensaje.show();
             }
-        } else {
-            Toast mensaje = Toast.makeText(this, "Las contraseña no coinciden", Toast.LENGTH_LONG);
-            mensaje.show();
         }
+
     }
+
     public void close( View view){
         Intent intent = new Intent(Register_activity.this, MainActivity.class);
         intent.putExtra("usuarios", user);
